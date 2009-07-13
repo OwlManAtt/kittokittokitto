0f64bc16b01a0e4e154d8fd14e4e49d6067be698
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2009-07-13 18:01:27 -0400

    Updated the DB dump tools.

--- db/bin/refresh_mysql.sh
+++ db/bin/refresh_mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tables=( jump_page item_class item_type pet_specie pet_specie_color cron_tab shop_restock board avatar timezone datetime_format staff_permission staff_group shop pet_specie_pet_specie_color board_category )
+tables=( jump_page item_class item_type pet_specie pet_specie_color cron_tab shop_restock board avatar timezone datetime_format staff_permission staff_group shop pet_specie_pet_specie_color board_category item_recipe_material item_recipe_type )
 
 if [ "$1" = '' ]
 then
@@ -7,9 +7,9 @@ then
     exit 2;
 fi
 
-mysqldump -d -u root --password=$1 kkk > mysql5_ddl.sql
+mysqldump -d -u root --password=$1 kitto > mysql5_ddl.sql
 
 for table in  ${tables[@]}
 do
-    mysqldump -t -c -u root --password=$1 kkk $table > data_mysql/${table}.sql
+    mysqldump -t -c -u root --password=$1 kitto $table > mysql_data/${table}.sql
 done
--- db/bin/refresh_postgresql.sh
+++ db/bin/refresh_postgresql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tables=( jump_page item_class item_type pet_specie pet_specie_color cron_tab shop_restock board avatar timezone datetime_format staff_permission staff_group shop pet_specie_pet_specie_color board_category )
+tables=( jump_page item_class item_type pet_specie pet_specie_color cron_tab shop_restock board avatar timezone datetime_format staff_permission staff_group shop pet_specie_pet_specie_color board_category item_recipe_material item_recipe_type ) 
 
 if [ "$1" = '' ]
 then
